9d136f1d128c5bd4f1ed884da36e0f1234a2bffc
tiktaak <aramina1414@gmail.com>
2023-12-15 09:09:14 +0330

    database basic setup

--- src/database.py
+++ src/database.py
@@ -1,8 +1,22 @@
 import sqlite3
 
 class Database(object):
+    db = None
+    cursor = None
     def __init__(self):
-        # db = sqlite3.connect("database/db.sqlite3")
-        db = "database"
-        
+        self.db = sqlite3.connect("database/db.sqlite3")
+        self.cursor = self.db.cursor()
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS post(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            TEXT NOT NULL
+            )
+        ''')
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS user(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            TEXT NOT NULL
+            )
+        ''')
+        self.db.commit()
         
